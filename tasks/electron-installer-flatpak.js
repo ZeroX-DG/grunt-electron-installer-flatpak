e44685e8ee9a2071cfe19e08ba00e0fdff6510e7
ZeroX-DG <viethungax@gmail.com>
2020-06-10 19:22:58 +1200

    used @malept/electron-installer-flatpak

--- tasks/electron-installer-flatpak.js
+++ tasks/electron-installer-flatpak.js
@@ -1,29 +1,30 @@
-'use strict'
+'use strict';
 
-var installer = require('electron-installer-flatpak')
+var installer = require('@malept/electron-installer-flatpak');
 
-module.exports = function (grunt) {
-  grunt.registerMultiTask('electron-installer-flatpak',
+module.exports = function(grunt) {
+  grunt.registerMultiTask(
+    'electron-installer-flatpak',
     'Create a Flatpak package for your Electron app.',
-    function () {
-      var done = this.async()
+    function() {
+      var done = this.async();
       var options = this.options({
         src: this.data.src,
         dest: this.data.dest,
-        rename: this.data.rename
-      })
+        rename: this.data.rename,
+      });
 
-      grunt.log.writeln('Creating package (this may take a while)')
+      grunt.log.writeln('Creating package (this may take a while)');
 
-      installer(options, function (err, options) {
+      installer(options, function(err, options) {
         if (!err) {
-          grunt.log.ok('Successfully created package at ' + options.dest)
+          grunt.log.ok('Successfully created package at ' + options.dest);
         } else {
-          grunt.log.error('Error creating package: ' + (err.message || err))
+          grunt.log.error('Error creating package: ' + (err.message || err));
         }
 
-        done(err)
-      })
-    }
-  )
-}
+        done(err);
+      });
+    },
+  );
+};
